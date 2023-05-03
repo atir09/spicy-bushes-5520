52e05b08732b1308f324e39e50f50386a7f0da1e
Swati Tanu <swatitani01@gmail.com>
2023-05-03 14:25:08 +0530

    basic admin route

--- Backend/routes/adminDashRouter.js
+++ Backend/routes/adminDashRouter.js
@@ -1,3 +1,18 @@
 const dashboardRouter = require("express").Router();
 
+
+
+
+
+dashboardRouter.get("/",(req,res)=>{
+    res.status(200).send({message:"Admin Page"})
+})
+
+
+
+
+
+
+
+
 module.exports = { dashboardRouter };
